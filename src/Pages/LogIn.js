2a8a5907f13f82dbb0361b501a3abd80a6d2995d
jennw8925 <emd2ty@virginia.edu>
2022-11-01 19:04:15 -0400

    11/1/22 update

--- src/Pages/LogIn.js
+++ src/Pages/LogIn.js
@@ -1,11 +1,11 @@
+import "../Styles/LogIn.css";
 import React from 'react';
 import {Link} from 'react-router-dom';
 
 const LogIn = () => {
     return(
     <div>
-        <b> Login </b>
-        
+        <b id = "login-text"> Login </b>
         <div>
             <Link to = "/"> <button>Back to Home</button></Link>
         </div>
